35e3e72653ff3da2ca6d479d5fb233e88b58d2ad
SoumyaD27 <soumyad233@gmail.com>
2022-11-19 14:52:43 +0530

    [Soumya]-21 times Display Head or Tail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
-
-for((i=1;i<=5;i++))
+a=0
+b=0
+for((i=1;i<=21;i++))
 do
  randomCheck=$((RANDOM%2))
   if [ $randomCheck -eq 1 ]
   then
     echo "Its Heads"
+    a=$((a + 1))
   else
     echo "Its Tails"
+    b=$((b + 1))
   fi
 done
+if [ $a -gt $b ]
+then
+   echo "Heads Win by $a times"
+elif [ $a -lt $b ]
+then
+   echo "Tails win by $b times"
+else
+   echo "Its a Tie"
+fi
